27cd0a88141b802c4e1f2ca83a398afa5df7d0d2
shenxl <shen@neuqsoft.com>
2020-07-29 09:52:15 +0800

    播放视频

---
+++ src/main/java/longs/api/api/NonStaticResourceHttpRequestHandler.java
@@ -0,0 +1,24 @@
+package longs.api.api;
+
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.resource.ResourceHttpRequestHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import java.nio.file.Path;
+
+/**
+ * @author Mr.S
+ * @date 2020/3/10
+ */
+@Component
+public class NonStaticResourceHttpRequestHandler extends ResourceHttpRequestHandler {
+    public final static String ATTR_FILE = "NON-STATIC-FILE";
+
+    @Override
+    protected Resource getResource(HttpServletRequest request) {
+        final Path filePath = (Path) request.getAttribute(ATTR_FILE);
+        return new FileSystemResource(filePath.toString());
+    }
+}
